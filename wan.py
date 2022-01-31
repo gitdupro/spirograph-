b9497b1a36b95e301fad2a447bc9c84f19c37bda
gitdupro <95160121+gitdupro@users.noreply.github.com>
2022-01-31 22:28:11 +0800

    Update wan.py

--- wan.py
+++ wan.py
@@ -1,13 +1,13 @@
 from turtle import up,down,setpos,mainloop,color
 from math import sin,cos,radians,fabs
-def clc(x,y,R):
+def clr(x,y,R):
 	up()
 	setpos(x+R,0)
 	down()
 	for t in range(1,361):
 		t=radians(t)
 		setpos(x+R*cos(t),y+R*sin(t))
-def wanin(x,y,R,r):
+def cycloidIn(x,y,R,r):
 	d=R-r
 	for t in range(1,10000):
 		t=radians(t)
@@ -18,7 +18,7 @@ def wanin(x,y,R,r):
 		setpos(x+dx+cx,y+dy+cy)
 		if fabs(dx+cx-R)<0.25 and fabs(dy+cy)<0.25:
 			break
-def wanout(x,y,R,r):
+def cycloidOut(x,y,R,r):
 	d=R+r
 	for t in range(1,10000):
 		t=radians(t)
@@ -34,14 +34,14 @@ color("red")
 R=500
 r=85
 x,y=0,0
-clc(x,y,R)
-wanin(x,y,R,r)
+clr(x,y,R)
+cycloidIn(x,y,R,r)
 
 x+=100
 R+=30
-clc(x,y,R)
-wanin(x,y,R,r)
+clr(x,y,R)
+cycloidOut(x,y,R,r)
 
-clc(50,50,350)
-wanout(50,50,350,50)
+clr(50,50,350)
+cycloidIn(50,50,350,50)
 mainloop()
